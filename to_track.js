5810cd5ebd00c6980a09c448567fdbaa00de4bc5
Adam Lofting <contactme@adamlofting.com>
2014-04-22 13:04:32 +0100

    track TogetherJS project

--- to_track.js
+++ to_track.js
@@ -1,5 +1,6 @@
 exports.projects = [
   { "name": "webmaker", "team": "webmaker" },
   { "name": "mozilla-webmaker-docs", "team": "webmaker" },
+  { "name": "mozilla-togetherjs", "team": "webmaker" },
   { "name": "mozilla-appmaker", "team": "appmaker" }
 ];
