fb56100c1760dfd4365cd17e4929b884d11282b7
Adam Lofting <contactme@adamlofting.com>
2014-06-06 14:49:00 +0100

    index

--- SQL/create.sql
+++ SQL/create.sql
@@ -1,6 +1,7 @@
 CREATE TABLE `activities` (
   `happened_on` datetime NOT NULL,
-  `user` varchar(255) DEFAULT NULL,
-  `mozilla_team` varchar(50) DEFAULT NULL,
-  UNIQUE KEY `happened_on` (`happened_on`,`user`,`mozilla_team`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+  `user` varchar(500) DEFAULT NULL,
+  `mozilla_team` varchar(255) DEFAULT NULL,
+  UNIQUE KEY `happened_on` (`happened_on`,`user`,`mozilla_team`),
+  KEY `idx` (`user`,`happened_on`,`mozilla_team`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
